7bddddeb01a81902f080099113f510e71f3466a9
Alex Sánchez <alejandro.sanchez.cabana@gmail.com>
2025-12-14 13:03:37 +0100

    Day 4 Problem 2

--- src/main/java/com/alex/advent/of/code/Main.java
+++ src/main/java/com/alex/advent/of/code/Main.java
@@ -3,10 +3,11 @@ package com.alex.advent.of.code;
 import com.alex.advent.of.code.commons.ProblemsBase;
 import com.alex.advent.of.code.day3.Day3Problem2;
 import com.alex.advent.of.code.day4.Day4Problem1;
+import com.alex.advent.of.code.day4.Day4Problem2;
 
 public class Main {
   public static void main(String[] args) {
-    ProblemsBase problem = new Day4Problem1();
+    ProblemsBase problem = new Day4Problem2();
     System.err.println(problem.run());
   }
 }
